269183f367f8312d7b37872cb0ed986d3e4ae55e
malethik <quintiliani.d@gmail.com>
2024-07-22 19:07:18 +0200

    visual fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 import { ProfileComponent } from './profile/profile.component';
+import { ContacsComponent } from './contacs/contacs.component';
 import { WorkComponent } from './work/work.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { ContacsComponent } from './contacs/contacs.component';
-import { FooterComponent } from './footer/footer.component';
+import { StudyComponent } from './study/study.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HeaderComponent,
+    StudyComponent,
+    FooterComponent,
     ProfileComponent,
+    ContacsComponent,
     WorkComponent,
     ProjectsComponent,
-    ContacsComponent,
-    FooterComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
---
+++ src/app/study/study.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-study',
+  standalone: true,
+  imports: [],
+  templateUrl: './study.component.html',
+  styleUrl: './study.component.css'
+})
+export class StudyComponent {
+
+}
