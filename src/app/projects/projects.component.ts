e679c2367caf1a9cb5604252ab044846981fd5f2
malethik <quintiliani.d@gmail.com>
2024-07-22 22:52:43 +0200

    added first project

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
-
+  constructor() {}
 }
