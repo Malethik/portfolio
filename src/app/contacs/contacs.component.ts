6ec1d57195ded0d5a25f095bf55501aa35384a94
Daniele <quintiliani.d@gmail.com>
2024-08-24 18:00:12 +0200

    added modal

--- src/app/contacs/contacs.component.ts
+++ src/app/contacs/contacs.component.ts
@@ -1,15 +1,18 @@
 import { Component, Inject, inject } from '@angular/core';
 import { EmailService } from '../core/service/email.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contacs',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contacs.component.html',
   styleUrl: './contacs.component.css',
 })
 export class ContacsComponent {
+  showModal: boolean = false;
+
   from: string = '';
   to: string = 'quintiliani.d@gmail.com';
   subject: string = '';
@@ -21,9 +24,15 @@ export class ContacsComponent {
     this.emailService
       .sendEmail(this.from, this.to, this.subject, this.text)
       .subscribe({
-        next: (response) => console.log('Email inviata con successo', response),
+        next: (response) => (
+          (this.showModal = true),
+          console.log('Email inviata con successo', response)
+        ),
         error: (error) =>
           console.error("Errore durante l'invio dell'email", error),
       });
+  }
+  closeModal() {
+    this.showModal = false;
   }
 }
