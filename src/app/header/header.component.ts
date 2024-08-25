62a43e96247ec66c24107f81daaa07236753f9bb
Daniele <quintiliani.d@gmail.com>
2024-08-25 13:20:34 +0200

    added burger menu (HTML, CSS and logic)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,44 @@
 import { ViewportScroller } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ClickOutsideDirective } from '../core/directive/click-out-side.directive';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ClickOutsideDirective],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  @Input() init: boolean | undefined;
+  @Output() opened = new EventEmitter();
+  active = false;
+
   constructor(private viewportScroller: ViewportScroller) {}
 
+  ngOnInit() {
+    this.active = this.init || false;
+  }
   scrollTo(elementId: string): void {
     this.viewportScroller.scrollToAnchor(elementId);
+  }
+
+  onBurgerClicked() {
+    this.active = !this.active;
+    this.toggleMenuVisibility();
+    this.opened.emit();
+  }
+
+  closeMenu() {
+    this.active = false;
+    this.toggleMenuVisibility();
+  }
+  private toggleMenuVisibility() {
+    const menu = document.querySelector('.menu');
+    if (this.active) {
+      menu?.classList.add('visible');
+    } else {
+      menu?.classList.remove('visible');
+    }
   }
 }
